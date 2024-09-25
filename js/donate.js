0c5665606ba3b417eb898a07550c211b69ea5b71
ayman175 <ayman175k@gmail.com>
2024-09-25 12:47:12 +0600

    complete the website

--- js/donate.js
+++ js/donate.js
@@ -1,137 +1,139 @@
-document.getElementById('first-donate-btn').addEventListener('click', function () {
+document
+	.getElementById("first-donate-btn")
+	.addEventListener("click", function () {
+		const addMoney = getInputFieldValueById("first-input-field");
+		const balance = parseFloat(getTextFieldValueById("first-amount"));
+		const totalAmount = parseFloat(getTextFieldValueById("total-balance"));
 
+		const newBalance = balance + addMoney;
+		document.getElementById("first-amount").innerText = newBalance;
 
-	const addMoney = getInputFieldValueById('first-input-field');
-	const balance = getTextFieldValueById('first-amount');
-	const totalAmount = getTextFieldValueById('total-balance')
+		const decreaseBalance = totalAmount - addMoney;
+		document.getElementById("total-balance").innerText = decreaseBalance;
 
+		if (isNaN(addMoney) || addMoney <= 0) {
+			alert("Invalid Donation Amount");
+			return;
+		} else if (addMoney > totalAmount) {
+			alert("Donation exceeds available balance");
+			return;
+		} else {
+			const myModal = document.getElementById("my_modal_1");
+			myModal.showModal();
+		}
 
-	const newBalance = balance + addMoney;
-	document.getElementById('first-amount').innerText = newBalance;
+		// History
+		const inputField = getInputFieldValueById("first-input-field");
+		const donationTime = new Date();
+		const donationPlace = getTextFieldValueById("first-place-name");
+		const transcation = document.getElementById("history");
+		transcation.innerHTML += `
+   <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+   <p class='font-bold text-xl'>${inputField} Taka is  ${donationPlace}</p>
+   <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+   </div>
+   `;
+	});
 
-	const decreaseBalance = totalAmount - addMoney;
-	document.getElementById('total-balance').innerText = decreaseBalance;
+document
+	.getElementById("second-donate-btn")
+	.addEventListener("click", function () {
+		const addMoney = getInputFieldValueById("second-input-field");
+		const balance = parseFloat(getTextFieldValueById("second-balance"));
+		console.log(balance);
+		const totalAmount = parseFloat(getTextFieldValueById("total-balance"));
+		console.log(totalAmount);
+		const newBalance = balance + addMoney;
+		document.getElementById("second-balance").innerText = newBalance;
 
+		const decreaseBalance = totalAmount - addMoney;
+		document.getElementById("total-balance").innerText = decreaseBalance;
 
+		if (isNaN(addMoney) || addMoney <= 0) {
+			alert("Invalid Donation Amount");
+			return;
+		} else if (addMoney > totalAmount) {
+			alert("Donation exceeds available balance");
+			return;
+		} else {
+			const myModal = document.getElementById("my_modal_1");
+			myModal.showModal();
+		}
 
-	if (isNaN(addMoney) || addMoney <= 0) {
-		alert('Invalid Donation Amount');
-		return;
-	}
-	else if (addMoney > totalAmount) {
-		alert('Donation exceeds available balance');
-		return;
-	}
-	else {
-		const myModal = document.getElementById('my_modal_1')
-		myModal.showModal();
-	}
+		// History
+		const inputField = getInputFieldValueById("second-input-field");
+		const donationTime = new Date();
+		const donationPlace = getTextFieldValueById("second-place-name");
+		const transcation = document.getElementById("history");
+		transcation.innerHTML += `
+   <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+   <p class='font-bold text-xl'>${inputField} Taka is  ${donationPlace}</p>
+   <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+   </div>
+   `;
+	});
 
-	// history
+document
+	.getElementById("third-donate-btn")
+	.addEventListener("click", function () {
+		const addMoney = getInputFieldValueById("third-input-field");
+		const balance = parseFloat(getTextFieldValueById("third-balance"));
+		const totalAmount = parseFloat(getTextFieldValueById("total-balance"));
 
-	const inputField = getInputFieldValueById('first-input-field');
-	const donationTime = new Date();
-	const donationPlace = getTextFieldValueById('first-place-name');
-	const transaction = document.getElementById('history');
-	transaction.innerHTML += `
-	<div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
-	<p class="font-bold text-xl">${inputField} Taka is ${donationPlace} </p>
-	<p class="text-sm text-gray-600 mt-2">date:${donationTime}</p>
-	</div>
-	`
-});
+		const newBalance = balance + addMoney;
+		document.getElementById("third-balance").innerText = newBalance;
 
-document.getElementById('second-donate-btn').addEventListener('click', function () {
+		const decreaseBalance = totalAmount - addMoney;
+		document.getElementById("total-balance").innerText = decreaseBalance;
 
-	const addMoney = getInputFieldValueById('second-input-field');
-	const balance = parseFloat(getTextFieldValueById('second-amount'));
+		if (isNaN(addMoney) || addMoney <= 0) {
+			alert("Invalid Donation Amount");
+			return;
+		} else if (addMoney > totalAmount) {
+			alert("Donation exceeds available balance");
+			return;
+		} else {
+			const myModal = document.getElementById("my_modal_1");
+			myModal.showModal();
+		}
 
-	const totalAmount = parseFloat(getTextFieldValueById('total-balance'));
+		// History
+		const inputField = getInputFieldValueById("third-input-field");
+		const donationTime = new Date();
+		const donationPlace = getTextFieldValueById("thrid-place-name");
+		const transcation = document.getElementById("history");
+		transcation.innerHTML += `
+   <div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
+   <p class='font-bold text-xl'>${inputField} Taka is  ${donationPlace}</p>
+   <p class='text-sm text-gray-600 mt-2'>Date: ${donationTime}</p>
+   </div>
+   `;
+	});
 
-	const newBalance = balance + addMoney;
-	document.getElementById('second-amount').onpointerenter = newBalance;
+//   History Button
 
-	const decreaseBalance = totalAmount - addMoney;
-	Document.getElementById('total-balance').innerText = decreaseBalance;
+function showbtn(id) {
+	document.getElementById("history").classList.add("hidden");
+	document.getElementById("donation").classList.add("hidden");
 
-	if (isNaN(addMoney) || addMoney <= 0) {
-		alert('Invalid Donation Amount');
-		return;
-	}
-	else if (addMoney > totalAmount) {
-		alert('Donation exceeds available balance');
-		return;
-	}
-	else {
-		const myModal = document.getElementById('my_modal_1')
-		myModal.showModal();
-	}
+	document.getElementById(id).classList.remove("hidden");
+}
+function showbtnColor(id) {
+	document.getElementById("history-add-btn").classList.remove("bg-lime-300");
+	document.getElementById("donation-add-btn").classList.remove("bg-lime-300");
+	document.getElementById(id).classList.add("bg-lime-300");
+}
 
-	// history 2
+document
+	.getElementById("donation-add-btn")
+	.addEventListener("click", function () {
+		showbtn("donation");
+		showbtnColor("donation-add-btn");
+	});
 
-	const inputField = getInputFieldValueById('second-input-field');
-	const donationTime = new Date();
-	const donationPlace = getTextFieldValueById('second-place-name');
-	const transaction = document.getElementById('history');
-	transaction.innerHTML += `
-	<div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
-	<p class="font-bold text-xl">${inputField} Taka is ${donationPlace} </p>
-	<p class="text-sm text-gray-600 mt-2">date:${donationTime}</p>
-	</div>
-	`
-});
-
-
-document.getElementById('third-donate-btn"').addEventListener('click', function () {
-	const addMoney = getInputFieldValueById('third-input-field');
-	const balance = parseFloat(getTextFieldValueById('third-amount'));
-
-	const totalAmount = parseFloat(getTextFieldValueById('total-balance'));
-
-	const newBalance = balance + addMoney;
-	document.getElementById('third-amount').onpointerenter = newBalance;
-
-	const decreaseBalance = totalAmount - addMoney;
-	Document.getElementById('total-balance').innerText = decreaseBalance;
-
-
-	if (isNaN(addMoney) || addMoney <= 0) {
-		alert('Invalid Donation Amount');
-		return;
-	}
-	else if (addMoney > totalAmount) {
-		alert('Donation exceeds available balance');
-		return;
-	}
-	else {
-		const myModal = document.getElementById('my_modal_1')
-		myModal.showModal();
-	}
-
-	const inputField = getInputFieldValueById('third-input-field');
-	const donationTime = new Date();
-	const donationPlace = getTextFieldValueById('third-place-name');
-	const transaction = document.getElementById('history');
-	transaction.innerHTML += `
-	<div class="border p-5 rounded-xl lg:w-4/5 w-11/12 mx-auto">
-	<p class="font-bold text-xl">${inputField} Taka is ${donationPlace} </p>
-	<p class="text-sm text-gray-600 mt-2">date:${donationTime}</p>
-	</div>
-	`
-
-});
-
-
-// history btn
-
-document.getElementById('donation-add-btn').addEventListener('click', function () {
-
-	showbtn('donation');
-	showbtnColor('donation-add-btn');
-});
-
-document.getElementById('donation-add-btn').addEventListener('click', function () {
-
-	showbtn('history');
-	showbtnColor('history-add-btn');
-});
+document
+	.getElementById("history-add-btn")
+	.addEventListener("click", function () {
+		showbtn("history");
+		showbtnColor("history-add-btn");
+	});
--- js/utilities.js
+++ js/utilities.js
@@ -1,31 +1,30 @@
 
 function getInputFieldValueById(id) {
-	const inputValue = document.getElementById(id).value;
-	const inputNumber = parseFloat(inputValue);
+	const inputValue = document.getElementById(id);
+	const inputNumber = parseFloat(inputValue.value);
 	return inputNumber;
-}
+};
 
 
 function getTextFieldValueById(id) {
 	const textValue = document.getElementById(id).innerText;
-	const textNumber = parseFloat(textValue);
-	return textNumber;
-}
+	return textValue;
+};
 
 // history button
 
-function showbtn(id) {
+function showBtn(id) {
 	document.getElementById('history').classlist.add('hidden');
 	document.getElementById('donation').classList.add('hidden');
 
 	document.getElementById(id).classList.remove('hidden');
-}
+};
 
-function showbtnColor(id) {
+function showBtnColor(id) {
 	document.getElementById('history-add-btn').classList.remove('bg-lime-300');
 	document.getElementById('donation-add-btn').classList.remove('bg-lime-300');
 
 	document.getElementById(id).classList.add('bg-lime-300');
-}
+};
 
 
